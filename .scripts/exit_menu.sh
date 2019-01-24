a4e883715d11f539ce2609e78ee224c914e6d20b
Frans Bergman <frans@tankernn.eu>
2019-01-24 23:46:46 +0100

    Add exit/shutdown script using dmenu

---
+++ .scripts/exit_menu.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+CHOICE=$(dmenu -i -b << EOF
+Lock
+Sleep
+Exit
+Shutdown
+Reboot
+EOF
+)
+
+case $CHOICE in
+    "Lock")
+        ~/.scripts/lock.sh
+        ;;
+    "Sleep")
+        ~/.scripts/lock.sh && echo mem | sudo /usr/bin/tee /sys/power/state
+        ;;
+    "Exit")
+        i3-msg exit
+        ;;
+    "Shutdown")
+        shutdown -h now
+        ;;
+    "Reboot")
+        reboot
+        ;;
+    *)
+        ;;
+esac
+
