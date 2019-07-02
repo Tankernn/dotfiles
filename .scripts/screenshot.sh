b7bbc136646238d971f540d2e437f872fe30ad82
Frans Bergman <frans@tankernn.eu>
2019-07-02 20:07:56 +0200

    Copy screenshot to clipboard instead of uploading

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-maim -us ~/screenshot.png
-
-UUID=$(uuidgen)
-UUID="${UUID[*]:0:5}"
-
-scp ~/screenshot.png "frans@tankernn.eu:/srv/www/scr/$UUID.png"
-
-echo "http://scr.tankernn.eu/$UUID.png" | xsel --clipboard
+maim -us | xclip -t image/png -selection clipboard
