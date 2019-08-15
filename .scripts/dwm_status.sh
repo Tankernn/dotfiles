e4afd75d9f825ad9de846bfaff68555065eb0310
Frans Bergman <frans@tankernn.eu>
2019-08-15 21:45:50 +0200

    Add playerctl metadata to dwm status

--- .scripts/dwm_status.sh
+++ .scripts/dwm_status.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 while true; do
+    MUSIC="$(playerctl metadata artist) - $(playerctl metadata title)"
     MEM="$(free -h | awk '(NR==2){ print $3 }')"
     VOL="$(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }')"
     TEMP="$(sensors | awk '/Package/{print $4}')"
     DATE="$(date)"
-    xsetroot -name " $VOL | $MEM | $TEMP | $DATE"
+    xsetroot -name " $MUSIC | $VOL | $MEM | $TEMP | $DATE"
     sleep 5
 done
