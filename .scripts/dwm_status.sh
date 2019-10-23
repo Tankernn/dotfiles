24a188012986ad249d0a2b0f7c1861228320c28c
Frans Bergman <frans@tankernn.eu>
2019-10-23 14:25:17 +0000

    Simplify date format in dwm_status.sh

--- .scripts/dwm_status.sh
+++ .scripts/dwm_status.sh
@@ -5,7 +5,7 @@ while true; do
     MEM="$(free -h | awk '(NR==2){ print $3 }')"
     VOL="$(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }')"
     TEMP="$(sensors | awk '/Package/{print $4}')"
-    DATE="$(date)"
+    DATE="$(date +"%F %T")"
     xsetroot -name " $MUSIC | $VOL | $MEM | $TEMP | $DATE"
     sleep 5
 done
