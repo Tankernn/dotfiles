34357a0b7692c1587a91f217194e971af0caf874
Frans Bergman <frans@tankernn.eu>
2019-12-27 20:53:44 +0100

    Use pulseaudio-ctl for displaying volume in dwm_status.sh

--- .scripts/dwm_status.sh
+++ .scripts/dwm_status.sh
@@ -3,7 +3,7 @@
 while true; do
     MUSIC="$(playerctl metadata artist) - $(playerctl metadata title)"
     MEM="$(free -h | awk '(NR==2){ print $3 }')"
-    VOL="$(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }')"
+    VOL="$(pulseaudio-ctl full-status | awk '{print $1 "%" ($2 == "yes" ? " (muted)" : "")}')"
     TEMP="$(sensors | awk '/Package/{print $4}')"
     DATE="$(date +"%F %T")"
     xsetroot -name " $MUSIC | $VOL | $MEM | $TEMP | $DATE"
