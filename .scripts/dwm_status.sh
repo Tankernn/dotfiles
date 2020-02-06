b33374d676f656e8e9b97a0854afa97d35490d65
Frans Bergman <frans@tankernn.eu>
2020-02-06 20:10:21 +0100

    Add support for mpd to dwm status script

--- .scripts/dwm_status.sh
+++ .scripts/dwm_status.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 while true; do
-    MUSIC="$(playerctl metadata artist) - $(playerctl metadata title)"
+    if playerctl metadata; then
+        MUSIC="$(playerctl metadata artist) - $(playerctl metadata title)"
+    else
+        MUSIC="$(mpc current)"
+    fi
     MEM="$(free -h | awk '(NR==2){ print $3 }')"
     VOL="$(pulseaudio-ctl full-status | awk '{print $1 "%" ($2 == "yes" ? " (muted)" : "")}')"
     TEMP="$(sensors | awk '/Package/{print $4}')"
