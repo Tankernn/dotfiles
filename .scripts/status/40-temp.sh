81d2e9be6471f7181a0f5c26014722c2f0037e1d
Frans Bergman <frans@tankernn.eu>
2020-04-08 13:34:17 +0200

    Add colors to temperature status section

--- .scripts/status/40-temp.sh
+++ .scripts/status/40-temp.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-sensors | awk '/Package/{print $4}'
+
+TEMP="$(sensors | awk '/Package/{print $4}')"
+NUMTEMP="$(echo "$TEMP" | tr -dc '0-9')"
+if [ "$NUMTEMP" -ge 700 ]; then
+    TEMPCOL=""
+elif [ "$NUMTEMP" -ge 500 ]; then
+    TEMPCOL=""
+else
+    TEMPCOL=""
+fi
+echo "$TEMPCOL$TEMP"
