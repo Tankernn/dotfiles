2f4d1c8c9ce382a8035b1f8ae2cb37f0f8bbd7d8
Frans Bergman <frans@tankernn.eu>
2020-10-26 10:29:16 +0100

    Use pipes instead of a temporary file in lock.sh

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -2,7 +2,5 @@
 
 playerctl pause
 
-scrot /tmp/lock.png
-convert /tmp/lock.png -paint 2 /tmp/lock.png
-i3lock -i /tmp/lock.png
-rm /tmp/lock.png
+SIZE="$(xrandr | awk -F, '/Screen 0/ { print $2 }' | awk '{print $2 "x" $4 }')"
+maim -u | convert - -paint 2 RGB:- | i3lock --raw "$SIZE":rgb -i /dev/stdin
