60eaed50d57674b563f6933d044d5a9010476f63
Frans Bergman <frans@tankernn.eu>
2021-03-01 01:03:57 +0100

    Use D-Bus for power management operations

--- .scripts/exit_menu.sh
+++ .scripts/exit_menu.sh
@@ -15,19 +15,45 @@ case $CHOICE in
         ~/.scripts/lock.sh; xset dpms force standby
         ;;
     "Sleep")
-        ~/.scripts/lock.sh && sudo zzz
+        ~/.scripts/lock.sh && dbus-send \
+            --system \
+            --print-reply \
+            --dest=org.freedesktop.login1 \
+            /org/freedesktop/login1 \
+            org.freedesktop.login1.Manager.Suspend \
+            boolean:false
         ;;
     "Hibernate")
-        ~/.scripts/lock.sh && sudo ZZZ
+        ~/.scripts/lock.sh && dbus-send \
+                  --system \
+                  --print-reply \
+                  --dest=org.freedesktop.login1 \
+                  /org/freedesktop/login1 \
+                  org.freedesktop.login1.Manager.Hibernate \
+                  boolean:false
         ;;
     "Exit")
         pkill Xorg
         ;;
     "Shutdown")
-        sudo poweroff
+        dbus-send \
+                  --system \
+                  --print-reply \
+                  --dest=org.freedesktop.login1 \
+                  /org/freedesktop/login1 \
+                  org.freedesktop.login1.Manager.PowerOff \
+                  boolean:false
+
         ;;
     "Reboot")
-        sudo reboot
+        dbus-send \
+                  --system \
+                  --print-reply \
+                  --dest=org.freedesktop.login1 \
+                  /org/freedesktop/login1 \
+                  org.freedesktop.login1.Manager.Reboot \
+                  boolean:false
+
         ;;
     *)
         ;;
